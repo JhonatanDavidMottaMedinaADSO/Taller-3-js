5c283cce375064f14b19d84a9e56e07974948b42
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-29 17:24:13 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 addEventListener("DOMContentLoaded", (e)=>{
     
-    // 12. Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en
-    // caso contrario indicar con un mensaje que la operación no es posible realizarla.
+    // 13. En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendo
+    // de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C.
+    // Los descuentos son los siguientes:
+    // Tipo A 10% de descuento
+    // Tipo B 15% de descuento
+    // Tipo C 20% de descuento
     
-    let num1 = Number(prompt(`Ingrese un valor 1`))
-    let num2 = Number(prompt(`Ingrese un valor 2`))
+    let memb = prompt(`¿Qué tipo de membresia tiene: A, B o C?`).toUpperCase()
 
-    if (num1 > num2) {
-        let operacion = num1 - num2
-        console.log(`El resultado de la sustracción es:`,operacion)
+    if (memb == `A`) {
 
-    } else {
-        console.log(`La operacion no es posible realizarla`)
+        alert (`Su descuento es del 10%`)
+        let product = Number(prompt(`Ingrese el valor del producto:`)) 
+        let des = product - (product * 0.1) 
+        console.log(`El valor a pagar con el descuento es de:`,des)
+
+    } else if (memb == `B`) {
+
+        alert (`Su descuento es del 15%`)
+        let product = Number(prompt(`Ingrese el valor del producto:`)) 
+        let des = product - (product * 0.15) 
+        console.log(`El valor a pagar con el descuento es de:`,des)
+
+    }
+    else if (memb == `C`) {
+
+        alert (`Su descuento es del 20%`)
+        let product = Number(prompt(`Ingrese el valor del producto:`)) 
+        let des = product - (product * 0.2) 
+        console.log(`El valor a pagar con el descuento es de:`,des)
+
     }
 })
