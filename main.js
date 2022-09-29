b4ba64485bb17b9cd44380a2146c3e2ad24521dd
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-29 16:07:22 -0500

    Ejercicio 9

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 addEventListener("DOMContentLoaded", (e)=>{
-
-    // 8. En cierta empresa se les paga a sus trabajadores de la siguiente forma: si el empleado es de planta,
-    // la hora trabajada se le paga a $20000, si el empleado es administrativo, la hora trabajada se le paga
-    // a $10000. Para calcular su pago es necesario conocer el total de horas trabajadas.
     
-    let rol = (prompt(`¿Es usted empleado de planta o administrativo?`)).toLowerCase()
+    // 9. Realizar el algoritmo que lea N números, calcule y escriba la suma de los pares y el producto de los
+    // impares. Numero par es aquel que su residuo igual a cero (num mod !=0)
+    
+    let sum  = 0
+    let producto = 1
+    let cant = Number(prompt(`Digite la cantidad de números que quiere ingresar`))
 
-    let hora = Number(prompt(`Ingrese la cantidad de horas trabajadas`))
+    for (let i = 0; i < cant; i++) {
 
-    if(rol == `planta`){
+        let num = Number(prompt(`Ingrese el número`))
 
-        let paga = hora * 20000
-        console.log(`Su pago será de`,paga)
+        if(num%2 == 0){
+
+            sum += num
         
-    } else if (rol == `administrativo`){
+        } else if (num%2 != 0) {
 
-        let paga = hora * 10000
-        console.log(`Su pago será de`,paga)
+            producto *= num
 
+        }
     }
+
+    console.log(`La suma de los pares es`,sum)
+    console.log(`El producto de los impares es`,producto)
     
 })
