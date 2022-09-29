a5771281ce331f614ae8bdc36d314de89fc31f44
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-09-29 14:58:21 -0500

    Ejercicio 5

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    // Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
-    // ¿Qué figura quiere calcular (Escriba T o C)?
-    // Triangulo = base * altura / 2
-    // Circulo = PI * radio* radio
+
+     // 5. Escriba un algoritmo que calcule el área de un rectángulo siempre y cuando los lados sean positivos:
+    // área triangulo= lado * lado.
     
-    alert (`Cálculo de áreas - Elige una figura geométrica: Triángulo o Círculo`)
-    let dec = (prompt(`¿Qué figura quiere calcular (Digite T o C):`))
+    let lado1 = Number(prompt(`Longitud de la base del rectángulo`))
+    let lado2 = Number(prompt(`Longitud de la altura del rectángulo`))
 
-    if(dec == `T`){
-        let base = Number(prompt(`Escribe la base del triangulo: `))
-        let height = Number(prompt(`Escribe la altura del triangulo: `))
-        let result = (base * height) / 2
-        console.log(`El area del triangulo es: `, result)
-
-    }
-    else if (dec == `C`) {
-        let radio = Number(prompt(`Escribe el radio del circulo`))
-        let result1 = Math.PI * Math.pow(radio, 2)
-        console.log(`El area del círculo es: `, result1)
+    if(lado1 > 0 && lado2 > 0){
+        let area = lado1 * lado2
+        console.log(`El area del rectangulo es:`,area)
     }
     
 })
